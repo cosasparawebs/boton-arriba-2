3cdcf7f4635eb9cb3931fe38747be95e6a1bb50a
cosasparawebs <66963970+cosasparawebs@users.noreply.github.com>
2020-06-15 13:50:25 -0300

    Add files via upload

---
+++ boton ir arriba (otra opcion JS)/js/script.js
@@ -0,0 +1,13 @@
+var irArriba = document.getElementsByClassName("ir-arriba");
+
+var irArriba = addEventListener("click", function(){
+    this.window.scrollTo(0, 0)
+})
+
+
+
+
+// en CSS esta colocado esto:---------------------
+// html {
+//     scroll-behavior: smooth;
+//   }
